4b96d2fd6a10bdd3c15a60819d69edd1e80e6e27
Гладнев Евгений Зурабович <evgeniy.gladnev@rt.ru>
2023-08-03 10:22:42 +0300

    ДЗ_10 готово

---
+++ src/main/java/ru/otus/hw10/Box.java
@@ -0,0 +1,73 @@
+package ru.otus.hw10;
+
+import java.util.Arrays;
+
+public class Box {
+    private final int size;
+    private String color;
+
+    private Boolean opened;
+
+    private String put;
+    String[] arr = new String[7];
+
+
+    public Box(int size, String color, Boolean opened, String put) {
+        this.size = size;
+        this.color = color;
+        this.opened = opened;
+        this.put = put;
+    }
+
+    public float getSize() {
+        return size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getOpened() {
+        return opened;
+    }
+
+    public void setOpened(Boolean opened) {
+        this.opened = opened;
+    }
+
+    public float getSize(float size) {
+        return size;
+    }
+
+    public void remArr(String put){
+        if (opened) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == put) {
+                    arr[i] = null;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void setArr(String put) {
+        if (opened) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == null) {
+                    arr[i] = put;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void info() {
+        String message = opened ? "Коробка открыта" : "Коробка закрыта";
+        System.out.println("Размер: " + size + " Цвет:  " + color + " Состояние: " + message + " Предметы: " + Arrays.asList(arr));
+        System.out.println();
+    }
+}
