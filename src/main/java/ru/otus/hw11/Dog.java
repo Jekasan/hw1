f9833b59440cfb4120c289c81dd0ece82fef26ed
Гладнев Евгений Зурабович <evgeniy.gladnev@rt.ru>
2023-08-15 12:39:56 +0300

    ДЗ_11 выполнено

---
+++ src/main/java/ru/otus/hw11/Dog.java
@@ -0,0 +1,7 @@
+package ru.otus.hw11;
+
+public class Dog extends Animal {
+    public Dog(String name,int runSpeed,int swimSpeed,int endurance) {
+        super(name,runSpeed,swimSpeed,endurance);
+    }
+}
---
+++ src/main/java/ru/otus/hw11/Horse.java
@@ -0,0 +1,7 @@
+package ru.otus.hw11;
+
+public class Horse extends Animal {
+    public Horse(String name,int runSpeed,int swimSpeed,int endurance) {
+        super(name,runSpeed,swimSpeed,endurance);
+    }
+}
