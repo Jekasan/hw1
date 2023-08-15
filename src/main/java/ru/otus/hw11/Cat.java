45c09fb0fb0c81527dd89b8293e05794af59e6da
Гладнев Евгений Зурабович <evgeniy.gladnev@rt.ru>
2023-08-15 12:48:36 +0300

    ДЗ_11 Выполнено

---
+++ src/main/java/ru/otus/hw11/Cat.java
@@ -0,0 +1,7 @@
+package ru.otus.hw11;
+
+public class Cat extends Animal {
+    public Cat(String name, int runSpeed, int swimSpeed, int endurance) {
+        super(name, runSpeed, swimSpeed, endurance);
+    }
+}
