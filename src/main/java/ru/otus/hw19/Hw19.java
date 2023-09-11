9176580bff450bd11f720a77b43469af76ddaee0
Rostelecom <jekasan87@gmail.com>
2023-09-12 02:23:49 +0300

    ДЗ_19 выполнено

--- src/main/java/ru/otus/hw19/Hw19.java
+++ src/main/java/ru/otus/hw19/Hw19.java
@@ -1,7 +1,63 @@
 package ru.otus.hw19;
 
 public class Hw19 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        long t = System.currentTimeMillis();
+        double[] array = new double[100_000_00];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+        }
+        System.out.println(System.currentTimeMillis() - t);
 
+        long ts = System.currentTimeMillis();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < (array.length * 25) / 100; i++) {
+                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = (array.length * 25) / 100; i < (array.length * 50) / 100; i++) {
+                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
+            }
+        });
+
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = (array.length * 50) / 100; i < (array.length * 75) / 100; i++) {
+                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
+            }
+        });
+
+        Thread t4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = (array.length * 75) / 100; i < array.length; i++) {
+                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+
+
+
+                System.out.println(System.currentTimeMillis() - ts);
     }
 }
