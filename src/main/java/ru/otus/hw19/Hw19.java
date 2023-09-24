d6ad5f21e6d95fa40c35d45e3078b14b648f8724
Rostelecom <jekasan87@gmail.com>
2023-09-24 21:30:22 +0300

    ДЗ_19 исправление 1

--- src/main/java/ru/otus/hw19/Hw19.java
+++ src/main/java/ru/otus/hw19/Hw19.java
@@ -1,6 +1,10 @@
 package ru.otus.hw19;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hw19 {
+    private static List<Thread> mThreadList;
     public static void main(String[] args) throws InterruptedException {
         long t = System.currentTimeMillis();
         double[] array = new double[100_000_00];
@@ -10,54 +14,27 @@ public class Hw19 {
         System.out.println(System.currentTimeMillis() - t);
 
         long ts = System.currentTimeMillis();
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < (array.length * 25) / 100; i++) {
-                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+
+        mThreadList = new ArrayList<Thread>();
+        for (int i = 0; i < 4; i++) {
+            int inc = i;
+            mThreadList.add(new Thread("Thread #" + i) {
+                @Override
+                public void run() {
+                    for (int i = (array.length / 4) * (inc); i < (array.length / 4) * (inc + 1); i++) {
+                        array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                    }
                 }
-            }
-        });
+            });
+        }
 
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = (array.length * 25) / 100; i < (array.length * 50) / 100; i++) {
-                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-                }
-            }
-        });
+        for (Thread currentThread : mThreadList) {
+            currentThread.start();
+        }
+        for (Thread currentThread : mThreadList) {
+            currentThread.join();
+        }
 
-        Thread t3 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = (array.length * 50) / 100; i < (array.length * 75) / 100; i++) {
-                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-                }
-            }
-        });
-
-        Thread t4 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = (array.length * 75) / 100; i < array.length; i++) {
-                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-                }
-            }
-        });
-
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-
-        t1.join();
-        t2.join();
-        t3.join();
-        t4.join();
-
-
-
-                System.out.println(System.currentTimeMillis() - ts);
+        System.out.println(System.currentTimeMillis() - ts);
     }
 }
